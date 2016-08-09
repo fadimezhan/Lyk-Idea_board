c6754336623607ff2f4311ed244000b7490838dd
Fadime Özhan <fadime_ozhan@hotmail.com>
2016-08-09 12:07:12 +0300

    Add Index To Category Name

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809071517) do
+ActiveRecord::Schema.define(version: 20160809085418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "description"
+    t.index ["name"], name: "index_categories_on_name", unique: true, using: :btree
   end
 
+  create_table "places", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "astablished_at"
+    t.string   "phone_number"
+    t.string   "contact_mail"
+    t.string   "city"
+    t.string   "description"
+    t.integer  "category_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["category_id"], name: "index_places_on_category_id", using: :btree
+  end
+
+  add_foreign_key "places", "categories"
 end
